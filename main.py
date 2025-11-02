eb37e02f20e7d0d8b71c9a900e18077c1fe36489
afkdev8 <mr.infinidy@protonmail.com>
2025-11-02 02:48:17 +0100

    Show duplicates

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class KeywordQueryEventListener(EventListener):
         items = []
         try:
             # 1. Execute shell command to list sessions
-            command = ["sesh", "l", "-c", "-t", "-T", "-d", "--json"]
+            command = ["sesh", "l", "-c", "-t", "-T", "--json"]
             process = subprocess.run(
                 command,
                 capture_output=True,
